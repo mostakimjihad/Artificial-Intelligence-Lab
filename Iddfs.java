6c8ebd2fd009552e2100bc9379e2874a0d430ad8
Mostakim JIhad <74228709+mostakimjihad@users.noreply.github.com>
2022-12-04 20:22:16 +0600

    updates ids code

--- Iddfs.java
+++ Iddfs.java
@@ -1,47 +1,36 @@
-package practice;
+package clp;
 import java.util.Scanner;
-
-public class Iddfs{
+public class Ids {
 	
 	static Scanner in = new Scanner(System.in);
-	static int[][] graph, time;
-	static int[] visited, parent;
-	static int e, v, t = 0;
+	static int[][] graph;
+	static int[] visited;
+	static int e, v;
 	
-	static boolean dfs(int source, int target, int limit) {
-		
-        visited[source] = 1;
-		
-		System.out.println("node = " + source + " limit = " + limit);
+	static boolean dls(int source, int target, int limit) {
 		
 		if(source == target)
 			return true;
 		if(limit <= 0)
 			return false;
 		
-		
-		
-		for(int i = 0; i < v; i++) {
-			
-			if(graph[source][i] == 1 && visited[i] != 1) {
-				
-				if(dfs(i, target, limit-1))
+		for(int i = 0; i < v; i++)
+			if(graph[source][i] == 1 && visited[i] != 1)
+				if(dls(i, target, limit-1))
 					return true;
-			}
-		}
 		
 		return false;
 	}
-	
-	static boolean iddfs(int source, int target, int max_depth) {
+	static boolean ids(int source, int target, int max_depth) {
 		
 		for(int i = 0; i < max_depth; i++) {
 			
 			for(int j = 0; j < v; j++)
 				visited[j] = 0;
-			System.out.println("limit value is " + i);
-			if(dfs(source, target, i))
+			
+			if(dls(source, target, i))
 				return true;
+			
 		}
 		return false;
 	}
@@ -50,30 +39,34 @@ public class Iddfs{
 		
 		System.out.println("Enter number vertices : ");
 		v = in.nextInt();
-		System.out.println("Enter number of edge : ");
+		
+		System.out.println("Enter number edges : ");
 		e = in.nextInt();
 		
 		graph = new int[v][v];
-		time = new int[v][2];
 		visited = new int[v];
-		parent = new int[v];
 		
 		for(int i = 0; i < e; i++) {
 			
-			System.out.println("Enter adjacency nodes: ");
+			System.out.println("Enter adjacency nodes :");
 			int x = in.nextInt();
 			int y = in.nextInt();
-			
 			graph[x][y] = 1;
 			graph[y][x] = 1;
-			
 		}
 		
-		if(iddfs(0, 3, 3))
-			System.out.println("found");
-		else
-			System.out.println("not found");
+		System.out.println("Enter source node : ");
+		int source = in.nextInt();
 		
+		System.out.println("Enter Max depth");
+		int max_depth = in.nextInt();
+		
+		System.out.println("Enter target : ");
+		int target = in.nextInt();
+		
+		if(ids(source,target, max_depth))
+			System.out.println("target node found");
+		else
+			System.out.println("target node not found");
 	}
-
 }
